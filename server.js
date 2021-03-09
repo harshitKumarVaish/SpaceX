9b1831051f214be18c7bb8d38e5a84f87d4d9ed8
Harshit Kumar Vaish <harshit.kumar@tekizma.com>
2021-03-09 11:34:31 +0530

    upd

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/space-x'));
+app.use(express.static(__dirname + '/dist/spaceX'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/space-x/index.html'));
+res.sendFile(path.join(__dirname+'/dist/spaceX/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
